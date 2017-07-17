23403f64d3fd1401e81e3b0427120b3565b869e6
Gregory Dukes <kcils360@gmail.com>
2017-07-17 13:35:30 -0700

    set up is complete

---
+++ apps.js
@@ -0,0 +1,18 @@
+'use strict';
+// open from 6:00 AM to 8:00 PM
+// //var location = {
+//     minCustsHr:  ,
+//     maxCustHr:  ,
+//     avgCoockiPerCust:  ,
+//     cookiesPerHr: [] ,
+//     custPertHr: [] ,
+//     totalCookiesDay:  ,
+//     (methods to calculate customer and cookie totals)
+//     (method to calcutlate customers per hour)
+
+var firstPike = {
+  minCustsHr: ,
+  maxCustHr: ,
+  avgCookiePerCust: ,
+  
+}
