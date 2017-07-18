e48348cbac150c7c3cc1f7422f702958b6033c2f
Gregory Dukes <kcils360@gmail.com>
2017-07-17 21:19:24 -0700

    all java complete and error free*

--- apps.js
+++ apps.js
@@ -12,7 +12,7 @@
 var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 //1st and Pike Location
-var pike = {
+var pikePlc = {
   minCustomers: 23,
   maxCustomers: 65,
   avgCookiesPerSale: 6.3,
@@ -39,19 +39,19 @@ var pike = {
     var pikeUL = document.getElementById('pike');
     for(var i = 0; i < hoursOpen.length; i++){
       var liEl = document.createElement('li');
-      liEl.textContent = hoursOpen[i] + ': ' + pike.hourlyCookies[i] + ' cookies';
+      liEl.textContent = hoursOpen[i] + ': ' + pikePlc.hourlyCookies[i] + ' cookies';
       pikeUL.appendChild(liEl);
     }
     var total = document.getElementById('pike');
     liEl = document.createElement('li');
-    liEl.textContent = 'total - ' + pike.cookiesPerDay(total);
+    liEl.textContent = 'total - ' + pikePlc.cookiesPerDay(total);
     total.appendChild(liEl);
   }
 };
 
-pike.customersPerHour();
-pike.cookiesPerHour();
-pike.render();
+pikePlc.customersPerHour();
+pikePlc.cookiesPerHour();
+pikePlc.render();
 
 //SeaTac Location
 var seaTac = {
@@ -78,11 +78,11 @@ var seaTac = {
     return total;
   },
   render: function(){
-    var pikeUL = document.getElementById('seatac');
+    var seatacUL = document.getElementById('seatac');
     for(var i = 0; i < hoursOpen.length; i++){
       var liEl = document.createElement('li');
       liEl.textContent = hoursOpen[i] + ': ' + seaTac.hourlyCookies[i] + ' cookies';
-      pikeUL.appendChild(liEl);
+      seatacUL.appendChild(liEl);
     }
     var total = document.getElementById('seatac');
     liEl = document.createElement('li');
@@ -119,11 +119,11 @@ var seaCent = {
     return total;
   },
   render: function(){
-    var pikeUL = document.getElementById('seacent');
+    var seacentUL = document.getElementById('seacent');
     for(var i = 0; i < hoursOpen.length; i++){
       var liEl = document.createElement('li');
       liEl.textContent = hoursOpen[i] + ': ' + seaCent.hourlyCookies[i] + ' cookies';
-      pikeUL.appendChild(liEl);
+      seacentUL.appendChild(liEl);
     }
     var total = document.getElementById('seacent');
     liEl = document.createElement('li');
@@ -160,11 +160,11 @@ var capHill = {
     return total;
   },
   render: function(){
-    var pikeUL = document.getElementById('caphill');
+    var caphillUL = document.getElementById('caphill');
     for(var i = 0; i < hoursOpen.length; i++){
       var liEl = document.createElement('li');
       liEl.textContent = hoursOpen[i] + ': ' + capHill.hourlyCookies[i] + ' cookies';
-      pikeUL.appendChild(liEl);
+      caphillUL.appendChild(liEl);
     }
     var total = document.getElementById('caphill');
     liEl = document.createElement('li');
@@ -175,3 +175,44 @@ var capHill = {
 capHill.customersPerHour();
 capHill.cookiesPerHour();
 capHill.render();
+
+//Alkai location
+var alKai = {
+  minCustomers: 2,
+  maxCustomers: 16,
+  avgCookiesPerSale: 4.6,
+  hourlyCookies: [],
+  customersPerHour: function(minCustomers, maxCustomers) {
+    return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers)) + this.minCustomers;
+  },
+  cookiesPerHour: function(){
+    for(var i = 0; i < hoursOpen.length; i++){
+      var cookiesPer = Math.floor(this.avgCookiesPerSale * this.customersPerHour());
+      this.hourlyCookies.push(cookiesPer);
+    }
+    return this.hourlyCookies;
+  },
+
+  cookiesPerDay: function(){
+    var total = 0;
+    for(var i = 0; i < hoursOpen.length; i++){
+      total += this.hourlyCookies[i];
+    }
+    return total;
+  },
+  render: function(){
+    var alkaiUL = document.getElementById('alkai');
+    for(var i = 0; i < hoursOpen.length; i++){
+      var liEl = document.createElement('li');
+      liEl.textContent = hoursOpen[i] + ': ' + alKai.hourlyCookies[i] + ' cookies';
+      alkaiUL.appendChild(liEl);
+    }
+    var total = document.getElementById('alkai');
+    liEl = document.createElement('li');
+    liEl.textContent = 'total - ' + alKai.cookiesPerDay(total);
+    total.appendChild(liEl);
+  }
+};
+alKai.customersPerHour();
+alKai.cookiesPerHour();
+alKai.render();
