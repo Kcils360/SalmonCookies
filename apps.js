0aff82754996f642bbe8804bcd38a15a0ecd6bc2
Gregory Dukes <kcils360@gmail.com>
2017-07-18 13:57:02 -0700

    constructor is working

--- apps.js
+++ apps.js
@@ -9,6 +9,7 @@
 //     totalCookiesDay:  ,
 //     (methods to calculate customer and cookie totals)
 //     (method to calcutlate customers per hour)
+var allStores = [];
 var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 function CookieStand(locationName, minCustomers, maxCustomers, avgCookiesPerSale){
@@ -16,7 +17,7 @@ function CookieStand(locationName, minCustomers, maxCustomers, avgCookiesPerSale
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
   this.avgCookiesPerSale = avgCookiesPerSale;
-  hourlyCookies = [];
+  this.hourlyCookies = [];
   this.customersPerHour = function(minCustomers, maxCustomers) {
     return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers)) + this.minCustomers;
   };
@@ -45,53 +46,54 @@ function CookieStand(locationName, minCustomers, maxCustomers, avgCookiesPerSale
     liEl = document.createElement('li');
     liEl.textContent = 'Total - ' + pikePlc.cookiesPerDay(total);
     total.appendChild(liEl);
-  }
+  };
+  allStores.push(this);
 };
-}
+
 
 //1st and Pike Location
-var pikePlc = {
-  minCustomers: 23,
-  maxCustomers: 65,
-  avgCookiesPerSale: 6.3,
-  hourlyCookies: [],
-  customersPerHour: function(minCustomers, maxCustomers) {
-    return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers)) + this.minCustomers;
-  },
-  cookiesPerHour: function(){
-    for(var i = 0; i < hoursOpen.length; i++){
-      var cookiesPer = Math.floor(this.avgCookiesPerSale * this.customersPerHour());
-      this.hourlyCookies.push(cookiesPer);
-    }
-    return this.hourlyCookies;
-  },
-
-  cookiesPerDay: function(){
-    var total = 0;
-    for(var i = 0; i < hoursOpen.length; i++){
-      total += this.hourlyCookies[i];
-    }
-    return total;
-  },
-  render: function(){
-    var pikeUL = document.getElementById('pike');
-    for(var i = 0; i < hoursOpen.length; i++){
-      var liEl = document.createElement('li');
-      liEl.textContent = hoursOpen[i] + ': ' + pikePlc.hourlyCookies[i] + ' cookies';
-      pikeUL.appendChild(liEl);
-    }
-    var total = document.getElementById('pike');
-    liEl = document.createElement('li');
-    liEl.textContent = 'Total - ' + pikePlc.cookiesPerDay(total);
-    total.appendChild(liEl);
-  }
-};
-new CookieStand(pikePlc, 23 ,65 ,6.3);
-
-pikePlc.customersPerHour();
-pikePlc.cookiesPerHour();
-pikePlc.render();
+// var pikePlc = {
+//   minCustomers: 23,
+//   maxCustomers: 65,
+//   avgCookiesPerSale: 6.3,
+//   hourlyCookies: [],
+//   customersPerHour: function(minCustomers, maxCustomers) {
+//     return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers)) + this.minCustomers;
+//   },
+//   cookiesPerHour: function(){
+//     for(var i = 0; i < hoursOpen.length; i++){
+//       var cookiesPer = Math.floor(this.avgCookiesPerSale * this.customersPerHour());
+//       this.hourlyCookies.push(cookiesPer);
+//     }
+//     return this.hourlyCookies;
+//   },
 //
+//   cookiesPerDay: function(){
+//     var total = 0;
+//     for(var i = 0; i < hoursOpen.length; i++){
+//       total += this.hourlyCookies[i];
+//     }
+//     return total;
+//   },
+//   render: function(){
+//     var pikeUL = document.getElementById('pike');
+//     for(var i = 0; i < hoursOpen.length; i++){
+//       var liEl = document.createElement('li');
+//       liEl.textContent = hoursOpen[i] + ': ' + pikePlc.hourlyCookies[i] + ' cookies';
+//       pikeUL.appendChild(liEl);
+//     }
+//     var total = document.getElementById('pike');
+//     liEl = document.createElement('li');
+//     liEl.textContent = 'Total - ' + pikePlc.cookiesPerDay(total);
+//     total.appendChild(liEl);
+//   }
+// };
+new CookieStand('pikePlc', 23 ,65 ,6.3);
+
+// pikePlc.customersPerHour();
+// pikePlc.cookiesPerHour();
+// pikePlc.render();
+// //
 // //SeaTac Location
 // var seaTac = {
 //   minCustomers: 3,
